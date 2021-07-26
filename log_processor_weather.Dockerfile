d9dffada0d799cd5a3d3817eaf4e1ea400071375
Dan <daniel.feely@gmail.com>
2021-07-26 11:01:21 +0100

    Gelocation

--- log_processor_weather.Dockerfile
+++ log_processor_weather.Dockerfile
@@ -10,12 +10,19 @@ RUN apt-get -y dist-upgrade
 
 # install python
 RUN apt-get install -y python$PYTHON_VERSION
+RUN apt-get install -y python3-pip
 RUN update-alternatives --install /usr/bin/python python /usr/bin/python$PYTHON_VERSION 1
 RUN update-alternatives --set python /usr/bin/python$PYTHON_VERSION
+
+# geoip lib
+RUN python -m pip install geoip2
 
 # copy source code
 RUN mkdir /opt/log_weather_processor
 COPY src/python /opt/log_weather_processor/src/python
+
+# mmdb
+COPY ./GeoLite2-City.mmdb /opt/log_processor_weather/GeoLite2-City.mmdb
 
 # entrypoint
 COPY ./entrypoint.sh /opt/log_processor_weather/entrypoint.sh
--- src/python/log_processor_weather.py
+++ src/python/log_processor_weather.py
@@ -4,6 +4,9 @@
 
 import sys
 import datetime
+import geoip2.database
+
+GEO_DB_PATH = "/opt/log_processor_weather/GeoLite2-City.mmdb"
 
 COUNTRY_ERRORS_COUNT = {}
 
@@ -17,7 +20,13 @@ def main():
     with open(sys.argv[1], "r") as file:
         handle_log_file(file)
 
+    sorted_words = sorted(
+        COUNTRY_ERRORS_COUNT.items(), key=lambda item: item[1], reverse=True
+    )
+    print(sorted_words)
 
+
+# iterate through log lines and handle 5xx errors
 def handle_log_file(file):
     for line in file:
         parsed_line = line.split(" ")
@@ -28,6 +37,7 @@ def handle_log_file(file):
             handle_5xx_error(parsed_line)
 
 
+# check if weekday and get geo info
 def handle_5xx_error(line):
     # convert string date from log to Python datetime
     log_date_format = "%d/%b/%Y:%H:%M:%S"
@@ -35,12 +45,31 @@ def handle_5xx_error(line):
 
     if check_weekday(datestamp):
         remote_ip = line[0]
+        try:
+            city = get_city_based_on_ip(remote_ip)
+            country = city.country.names["en"]
+            increment_country_count(country)
+
+        except:
+            # handle ip not in the database
+            print(f"Can't look up {remote_ip}")
+
+
+def increment_country_count(country):
+    if country not in COUNTRY_ERRORS_COUNT:
+        COUNTRY_ERRORS_COUNT[country] = 1
+    else:
+        COUNTRY_ERRORS_COUNT[country] = COUNTRY_ERRORS_COUNT[country] + 1
+
+
+def get_city_based_on_ip(ip):
+    with geoip2.database.Reader(GEO_DB_PATH) as reader:
+        return reader.city(ip)
 
 
 def check_weekday(datestamp):
     if datestamp.strftime("%A") in ["Saturday", "Sunday"]:
         return False
-    print(datestamp.strftime("%A"))
     return True
 
 
